285839e34973d0268b92dbe6f4b1c667c299dbdc
korban-mantan <mubarokakbar13@gmail.com>
2023-05-05 18:55:51 +0700

    new changes idk

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,11 @@ const router = createRouter({
       component: () => import('../views/HomeView.vue')
     },
     {
+      path: '/consult',
+      name: 'consult',
+      component: () => import('../views/ConsultView.vue')
+    },
+    {
       path: '/vaccination',
       name: 'vaccination',
       component: () => import('../views/VaccineView.vue')
@@ -24,9 +29,9 @@ const router = createRouter({
       component: () => import('../views/HospitalView.vue')
     },
     {
-      path: '/consult',
-      name: 'consult',
-      component: () => import('../views/ConsultView.vue')
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
     }
   ]
 })
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router'
 
 const username = ref('');
 const password = ref('');
@@ -7,7 +8,7 @@ const errorMessage = ref('');
 const loggedIn = ref(false);
 
 async function submitForm() {
-  const response = await fetch('http://localhost:8000/api/auth/login', {
+  const response = await fetch('http://localhost:8000/api/register', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username: username.value, password: password.value })
@@ -16,18 +17,20 @@ async function submitForm() {
   if (response.ok) {
     loggedIn.value = true;
     errorMessage.value = '';
-    
+
   } else {
     loggedIn.value = false;
     errorMessage.value = 'Incorrect username or password';
   }
+
+  useRouter.push('/login')
 }
 </script>
 
 <template>
   <div class="w-screen h-screen grid place-items-center">
     <form @submit.prevent="submitForm">
-      <h1 class="text-4xl cursor-default hover:text-teal-400">Login Vaccinify</h1>
+      <h1 class="text-4xl cursor-default hover:text-teal-400">Register Vaccinify</h1>
       <div
         class="grid gap-4 grid-cols-1 place-items-center bg-slate-50 border-teal-500 border-4 p-20 rounded-xl"
       >
@@ -42,16 +45,19 @@ async function submitForm() {
         />
         <label for="password">Password</label>
         <input
-          type="password"
-          placeholder="Insert you password"
-          name="password"
-          class="bg-transparent border-slate-500 border-2 rounded-lg p-2"
-          v-model="password"
-          required
+        type="password"
+        placeholder="Insert you password"
+        name="password"
+        class="bg-transparent border-slate-500 border-2 rounded-lg p-2"
+        v-model="password"
+        required
         />
-        <router-link to="/register">Register Now</router-link>
+        <label for="role">1</label>
+        <input type="radio" name="role" value="1">
+        <label for="role">2</label>
+        <input type="radio" name="role" value="2">
         <!-- <router-link to="/home"> -->
-          <button class="bg-teal-300 text-white w-40 p-2 rounded-lg">Login</button>
+        <button class="bg-teal-300 text-white w-40 p-2 rounded-lg">Login</button>
         <!-- </router-link> -->
       </div>
     </form>
